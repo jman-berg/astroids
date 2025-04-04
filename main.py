203e32c874ae2537a1f690d01f64660a133ac477
jman-berg <jogchumm@gmail.com>
2025-04-04 13:30:51 +0200

    added the player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -11,6 +12,7 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -19,6 +21,7 @@ def main():
         screen.fill(
             "black"
         )
+        player.draw(screen)
         pygame.display.flip()
 
         #End of Gameloop 
