bce12e75dc00b08fe9bc2c3a4877191651ca1b33
jman-berg <jogchumm@gmail.com>
2025-04-04 13:08:16 +0200

    added game clock and fps limit

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,12 @@ def main():
             "black"
         )
         pygame.display.flip()
+
+        #End of Gameloop 
+        delta_time = game_clock.tick(60) #tick 60 limits the FPS to 60
+        dt = delta_time / 1000 #convert to ms
+
+
 
 if __name__ == "__main__":
     main()
